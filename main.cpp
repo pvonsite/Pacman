a08d67fa6ae57dd85a1dd80fd583b0b0b5c5f6e2
PhongVan <fmlphongvan@gmail.com>
2021-02-20 01:49:09 +0700

    main updated

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include "Window.h"
 
+int main(int argc, char* argv[]) {
+
+    Window* window = new Window();
+
+    window->initSDL();
+
+    window->runGame();
+
+    window->quitSDL();
+    return 0;
+}
