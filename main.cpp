3d82e04f9475cc01c217986b57acdc2a7477394e
PhongVan <fmlphongvan@gmail.com>
2021-02-20 00:25:20 +0700

    Rewrite project

--- main.cpp
+++ main.cpp
@@ -1,18 +1,1 @@
-#include "GameEngine.h"
-#include <iostream>
 
-using namespace std;
-
-int main(int argc, char* argv[]) {
-
-    GameEngine::initSDL();
-
-    //GameEngine::initTexture();
-
-    while (GameEngine::getRunStatus()) {
-        GameEngine::handleEvent();
-    }
-
-    GameEngine::quitSDL();
-    //return 0;
-}
